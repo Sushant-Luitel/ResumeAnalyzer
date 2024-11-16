dda39cb082eb6161b1193eed553c2d6c9396b734
Sushant-Luitel <sushant.luitel@globalsquareit.com>
2024-11-16 20:36:59 +0545

    feat(file-upload): file upload-ui

--- src/components/FileDropZone/MyDropZone.tsx
+++ src/components/FileDropZone/MyDropZone.tsx
@@ -1,19 +1,47 @@
-import React, { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import styles from "./MyDropZone.module.css";
 import cloudUpload from "../../assets/cloud-sm.png";
 
 function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const [files, setFiles] = useState<any>([]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  if (files.length > 0) console.log(files, "files");
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
+      {files.length > 0 ? (
+        <div>
+          <img
+            src={files[0]?.preview}
+            className={styles["file-preview"]}
+            alt=""
+          />
+        </div>
+      ) : isDragActive ? (
+        <div className={styles["file-upload-outer-box"]}>
+          <div className={styles["file-upload-inner-box"]}>
+            <img
+              src={cloudUpload}
+              className={styles["file-upload-image"]}
+              alt=""
+            />
+            <p className={styles["file-upload-text"]}>drop your file here...</p>
+          </div>
+        </div>
       ) : (
         <div className={styles["file-upload-outer-box"]}>
           <div className={styles["file-upload-inner-box"]}>
