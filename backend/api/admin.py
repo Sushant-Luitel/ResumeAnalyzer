2627cfa825f8ada69b349dbe3ec66f3a316d6d42
Rikikesh <adarshamishra98@gmail.com>
2024-11-24 20:15:07 +0545

    added backend

---
+++ backend/api/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser,FileUpload
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('date_joined',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(FileUpload)
