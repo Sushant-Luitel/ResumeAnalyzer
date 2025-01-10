d1bb05c372358cad9afdbb491968293d725490a3
Rikikesh <adarshamishra98@gmail.com>
2025-01-10 18:22:43 +0545

    Fixed cors error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,10 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
-CORS_ALLOWED_ORIGINS=all
+
+CORS_ALLOWED_ORIGINS=['http://127.0.0.1:8000']
 AUTH_USER_MODEL = 'api.CustomUser' #registering custom user model
 
 ROOT_URLCONF = 'backend.urls'
