edc29222277587a7c2931d5e655707a30eed8784
Sushant-Luitel <sushant.luitel@globalsquareit.com>
2025-02-06 14:10:48 +0545

    fix(homepage): remove duplicate navbar

--- frontend/src/components/ResumeUpload/ResumeUpload.tsx
+++ frontend/src/components/ResumeUpload/ResumeUpload.tsx
@@ -1,11 +1,9 @@
 import MyDropzone from "../FileDropZone/MyDropZone";
-import Navbar from "../Navbar/Navbar";
 import styles from "./ResumeUpload.module.css";
 
 const ResumeUpload = () => {
   return (
     <div className={styles["resume-upload-container"]}>
-      <Navbar/>
       <h1 className="">AI Resume Analyzer:</h1>
       <h1 className="text-3xl"> Get Your Resume Score Now.</h1>
       <MyDropzone />
