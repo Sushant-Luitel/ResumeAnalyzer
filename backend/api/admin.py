10073796ee5671ed920ed44d1213ec1beb5fe937
Rikikesh Mishra <adarshamishra98@gmail.com>
2025-03-13 17:15:17 +0545

    fix(resume): download resume

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser,FileUpload,SavedJob,Recruiter,Job
+from django.utils.html import format_html
 # Register your models here.
 # class CustomUserAdmin(UserAdmin):
 #     model = CustomUser
@@ -14,9 +15,26 @@ from .models import CustomUser,FileUpload,SavedJob,Recruiter,Job
 @admin.register(SavedJob)
 class SavedJobAdmin(admin.ModelAdmin):
     # verbose_name="Job Application"
-    list_display = ('user', 'job', 'status')
+    list_display = ('user', 'job', 'status','view_resume','download_resume')
     list_filter = ('status',)
     list_editable = ('status',)
+
+    def view_resume(self, obj):
+        """Display a link to view the resume."""
+        file = FileUpload.objects.filter(user=obj.user).first()
+        if file and file.file:
+            return format_html('<a href="{}" target="_blank">View Resume</a>', file.file.url)
+        return "No Resume"
+
+    def download_resume(self, obj):
+        """Display a download link for the resume."""
+        file = FileUpload.objects.filter(user=obj.user).first()
+        if file and file.file:
+            return format_html('<a href="{}" download>Download Resume</a>', file.file.url)
+        return "No Resume"
+
+    view_resume.short_description = "Resume"
+    download_resume.short_description = "Download"
     
 
 # Job Admin
